abf1ebb89b2f4bd390fb10051857c1e2e729ab9f
Romain Giacalone <romain.giacalone@easiware.fr>
2020-10-02 17:44:00 +0200

    Refactor according to conventions

---
+++ functions/src/index.ts
@@ -0,0 +1,13 @@
+import "source-map-support/register";
+import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import { HTTPGetHandler } from "./http-get-handler";
+
+export const handler: Handler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  try {
+    const httpGetHandler = new HTTPGetHandler();
+    const response = await httpGetHandler.handle(event);
+    return callback(null, response);
+  } catch ( exception ) {
+    return callback(exception, event);
+  }
+}
