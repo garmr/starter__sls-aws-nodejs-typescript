d5a2794d74fa6d85013ea2bf54f8f658a7e9c499
Romain Giacalone <romain.giacalone@easiware.fr>
2020-10-03 08:17:39 +0200

    Attempt to fix WebpackOptionsValidationError: Invalid configuration object. Webpack has been initialised using a configuration object that does not match the API schema.

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,9 @@ const nodeExternals = require('webpack-node-externals');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
+  entry: slsw.lib.entries,
   context: __dirname,
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  entry: {
-    app: slsw.lib.entries
-  },
   devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
   resolve: {
     extensions: ['.mjs', '.json', '.ts'],
@@ -21,7 +19,9 @@ module.exports = {
     filename: '[name].js',
   },
   target: 'node',
-  externals: [nodeExternals()],
+  externals: [
+    nodeExternals()
+  ],
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
